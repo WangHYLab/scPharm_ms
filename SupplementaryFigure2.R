febc0e028e02b881a3955b2d5ca2dec88552a556
WangHYLab <42341884+WangHYLab@users.noreply.github.com>
2024-10-18 14:32:20 +0800

    Update SupplementaryFigure2.R

--- SupplementaryFigure2.R
+++ SupplementaryFigure2.R
@@ -1,41 +1,147 @@
 # Title    : Supplementary Figure 2
 # Author   : Wanglab
-# Time     : 2024.7
+# Time     : 2024.9
 
-library(Cairo)
 
-sample = c("AH0308", "MH0031", "MH0069", "MH0161", "MH0176", "PM0337")
-names(sample) = sample
+# Supplemental Figure 2a-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Function = function(sample,  drug_id, drug_name, cell_label){
-  fig3.data.1 = lapply(sample, function(sam) {
-    # Load the single-cell pharmacology result object from the specified path
-    rank = readRDS(paste0("./scPharm/result/", sam, "_scPharm_object_nmcs_50_nfs_200.rds"))
-    return(rank)
-  })
-  # Loop through each element in the fig3.data.1 list to generate a PDF chart for each
-  for (i in 1:6) {
-    meta.data = fig3.data.1[[i]]@meta.data
-    temp = paste("scPharm_nes", drug_id, drug_name, sep = "_")
-    meta.data = meta.data[,c("cell.label", temp)]
-    # Filter cells where the label == cell_label
-    meta.data = meta.data[meta.data$cell.label == cell_label,]
-    colnames(meta.data) = c("Group", "NES")
-    # Create a PDF file using the CairoPDF function, specifying filename, width, and height
-    CairoPDF(paste0("./Figure/SuppleFig2", drug_name, names(fig3.data.1)[i],"20231117.pdf", sep="_"), width = 2, height = 1.4)
-    # Set graphic parameters including outer margins, inner margins, and x-axis tick label positioning
-    par(oma = c(1.2, 3, 0.2, 1), mar = c(1, 0, 1.4, 0), xpd = TRUE)
-    plot(density(null$NES), col = "#999999", main = "",
-        lwd = 1.5, lty = 2, cex.axis = 1.2, font.main = 1, tcl = -0.25, ylab = "", xlab = "",
-        bty = "l", las = 1, ylim = c(0, 1.2))
-    lines(density(meta.data$NES), col = "#FF0000", lwd=1.5)
-    dev.off()
-  }
+# load results using scPharm in various Cell-ID size
+nfeatures = readRDS("./rscript/scPharm/nfeatures.rds")
+rank_ct = data.frame(matrix(0, nrow = 295, ncol = 30))
+colnames(rank_ct) = paste(names(nfeatures),
+                          c(rep("100",6), rep("150", 6), rep("200", 6), rep("250", 6), rep("300", 6)), sep = "_")
+names(nfeatures) = colnames(rank_ct)
+
+for (sam in names(nfeatures)) {
+  data = nfeatures[[sam]][["rank_ct"]]
+  rank_ct[data[data$DRUG_ID == 1032,]$Rank, sam] = 1
+  rank_ct[data[data$DRUG_ID == 1549,]$Rank, sam] = 2
+  rank_ct[data[data$DRUG_ID == 1558,]$Rank, sam] = 3
 }
-# SuppleFigure2a,b-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Function(sample, 1558, "Lapatinib", "tumor")
-Function(sample, 1558, "Lapatinib", "adjacent")
+library(ComplexHeatmap)
+library(circlize)
+row_info = data.frame(sample = names(nfeatures),
+                      group = c(rep("100",6), rep("150", 6), rep("200", 6), rep("250", 6), rep("300", 6)))
+rownames(row_info) = row_info$sample
+row_anno = HeatmapAnnotation(
+  df = row_info$group,
+  which = "row",
+  col = list(
+    df = c("100" = "#ABC6E4",
+           "150" = "#C39398",
+           "200" = "#FCDABA",
+           "250" = "#A7D2BA",
+           "300" = "#D0CADE"),
+    show_legend = TRUE
+  ),
+  show_annotation_name = FALSE,
+  annotation_label = "Cell-ID size",
+  annotation_legend_param = list(
+    color_bar = "discrete",
+    at = c("100","150","200","250","300"),
+    labels = c("100","150", "200", "250","300"),
+    legend_gp = gpar(fill = c("#ABC6E4","#C39398", "#FCDABA", "#A7D2BA", "#D0CADE")),
+    labels_gp = gpar(col = "#000000", fontsize = 12),
+    title_gp = gpar(fontsize = 12)
+  )
+)
 
-# SuppleFigure2c,d-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Function(sample, 1549, "Sapitinib", "tumor")
-Function(sample, 1549, "Sapitinib", "adjacent")
+Cairo::CairoPDF("./Rplot/review/copykat_true.pdf", width = 10, height = 5)
+Heatmap(t(rank_ct),
+        # row_split = 1:4,
+        # rect_gp = gpar(col = 'white', lwd = 0.1),
+        row_split = row_info$group,
+        row_gap = unit(1, "mm"),
+        col = colorRamp2(c(0,1,2,3), c("grey", "#E41A1C", "#377EB8", "#4DAF4A")),
+        # border = "black",
+        column_title = "Rank of all drugs(Dr)",
+        column_title_gp = gpar(fontsize = 14),
+        cluster_rows = F, 
+        cluster_columns = F,
+        show_row_names = T,
+        show_column_names = F,
+        row_names_gp = gpar(fontsize = 12),
+        row_names_side = "left",
+        row_title = NULL,
+        show_heatmap_legend = T,
+        name = "Drug",
+        heatmap_legend_param = list(
+          color_bar = "discrete",
+          at = 0:3,
+          labels = c("other","Afatinib", "Sapitinib", "Lapatinib"),
+          legend_gp = gpar(fill = c("grey","#E41A1C", "#377EB8", "#4DAF4A")), 
+          labels_gp = gpar(col = "#000000", fontsize = 12),
+          title_gp = gpar(fontsize = 12)
+        ),
+        left_annotation = row_anno,
+        row_labels = sapply(strsplit(names(nfeatures), split = "_"), function(x) x[1])
+        # width = unit(15, "cm"), height = unit(1.5, "cm")
+)
+dev.off()
+
+
+# Supplemental Figure 2b --------------------------------------------------
+
+## the up panel of Supplemental Figure 2b is from Figure 3d
+
+# Supplemental Figure 2b bottom panel
+
+sample = c("AH0308","MH0031","MH0069","MH0161","MH0176","PM0337")
+names(sample) = sample
+# load HER-positive sample result
+GSE161529_HER2 = lapply(sample, function(sam){
+  object = readRDS(paste0("./rscript/scPharm/result/", sam, "_scPharm1208_nmcs_50_nfs_200.rds"))
+})
+
+new_cut = lapply(GSE161529_HER2, function(object){
+  meta.data = object@meta.data
+  for (i in seq(7, ncol(meta.data), 2)) {
+    meta.data[,i] = "other"
+    meta.data[meta.data[,i+1] < -1.452096, i] = "sensitive"
+    meta.data[meta.data[,i+1] > 1.43721, i] = "resistant"
+  }
+  temp = calcu_rank(meta.data)
+})
+library(ComplexHeatmap)
+library(circlize)
+supplefig2b_bot = data.frame(matrix(0, nrow = nrow(new_cut[[1]]), ncol = length(new_cut)))
+colnames(supplefig2b_bot) = names(new_cut)
+for (sam in names(new_cut)) {
+  data = new_cut[[sam]]
+  supplefig2b_bot[data[data$DRUG_ID == 1032,]$Rank, sam] = 1
+  supplefig2b_bot[data[data$DRUG_ID == 1549,]$Rank, sam] = 2
+  supplefig2b_bot[data[data$DRUG_ID == 1558,]$Rank, sam] = 3
+}
+
+suppleFig2b = Heatmap(t(supplefig2b_bot),
+                      # row_split = 1:4,
+                      # rect_gp = gpar(col = 'white', lwd = 0.1),
+                      # row_split = group,
+                      # row_gap = unit(1, "mm"),
+                      col = colorRamp2(c(0,1,2,3), c("grey", "#E41A1C", "#377EB8", "#4DAF4A")),
+                      # border = "black",
+                      column_title = "Rank of all drugs(Dr)",
+                      column_title_gp = gpar(fontsize = 14),
+                      cluster_rows = F, 
+                      cluster_columns = F,
+                      show_row_names = T,
+                      show_column_names = F,
+                      row_names_gp = gpar(fontsize = 12),
+                      row_names_side = "left",
+                      row_title = "",
+                      show_heatmap_legend = T,
+                      name = "Drug",
+                      heatmap_legend_param = list(
+                        color_bar = "discrete",
+                        at = 0:3,
+                        labels = c("other","Afatinib", "Sapitinib", "Lapatinib"),
+                        legend_gp = gpar(fill = c("grey","#E41A1C", "#377EB8", "#4DAF4A")), 
+                        labels_gp = gpar(col = "#000000", fontsize = 12),
+                        title_gp = gpar(fontsize = 12)
+                      )
+                      # width = unit(15, "cm"), height = unit(1.5, "cm")
+)
+Cairo::CairoPDF("./Rplot/review/figures2_bot.pdf", width = 10, height = 1.6)
+print(suppleFig2b)
+dev.off()
+
